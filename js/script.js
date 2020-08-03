e9fba5b7b2959186bb509227ecdf912d65bffe40
Martina <martinapolitano23@gmail.com>
2020-08-03 17:18:36 +0200

    Fine

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Chiedo il nome all'utente e lo salvo in una variabile
+var nome = prompt("Inserisci nome");
+// Chiedo il cognome all'utente e lo salvo in una variabile
+var cognome = prompt("Inserisci cognome");
+// Chiedo il colore preferito all'utente e lo salvo in una variabile
+var colorePreferito = prompt("Inserisci colore preferito");
+// Stampa su video il risultato
+var somma = nome + cognome + colorePreferito + "19";
+console.log(somma);
